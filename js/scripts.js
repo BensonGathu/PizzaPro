2303f46860d8a700f82b41845e13c70053c1383a
Benson <bensongathu23@gmail.com>
2021-04-23 09:34:10 +0300

    img folder created and images added. Address collection form added

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+function Pizza(size,crust,toppings,num){
+    this.size = size;
+    this.toppings = toppings;
+    this.crust = crust;
+    this.num = num;
+    this.addresses = []
+}
+function Address(){
+
+}
+
+Pizza.prototype.fullDetails = function(){
+    
+}
+
+$(document).ready(function(){
+
+    $("form").submit(function(event){
+
+        event.preventDefault()
+        var size = document.getElementById("size")
+        var crust = document.getElementById("crust")
+        var toppings = document.getElementById("toppings")
+        var num = document.getElementById("num").value
+        var size = size.options[size.selectedIndex].text
+        var crust = crust.options[crust.selectedIndex].text
+        var toppings = toppings.options[toppings.selectedIndex].text
+        var pizza = new Pizza(size,crust,toppings,num)
+        console.log(pizza)
+    })
+    $("input#yes").click(function(){
+        $("#address").show()
+    })
+    $("input#no").click(function(){
+        $("#address").hide()
+    })
+
+})
